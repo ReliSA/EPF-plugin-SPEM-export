64c92a7bf6d9a76ca4369d6e88b393d431741250
Lenka Simeckova <simeckova.lenka@seznam.cz>
2020-05-14 13:30:54 +0200

    removed unused imports

--- cz.zcu.kiv.epf.spade.export.pattern.ui/src/cz/zcu/kiv/epf/spade/export/pattern/ui/wizards/SelectExportOptionsPage.java
+++ cz.zcu.kiv.epf.spade.export.pattern.ui/src/cz/zcu/kiv/epf/spade/export/pattern/ui/wizards/SelectExportOptionsPage.java
@@ -9,13 +9,10 @@ import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Group;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
